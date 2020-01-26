4ee54f840f02f1a843fcdf1c9953555afe9d3d04
Matthew Jacob <github@feral.com>
2020-01-26 11:12:13 -0800

    Update README.md and add source

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # fake-dedup
 simple python script to perform de-duplication on mostly identical directories
+
+This is a somewhat limited use case and not well tested. It came about
+because I was keeping about 2.5TiB of nearly identical buildroot build
+trees. I was hanging onto the last 6 months of builds of each commit
+for the place I worked.
+
+Now, disk space has gotten very cheap, but this had a ridiculous amount of
+identical data and files and it annoyed me to consume this much. Even if
+I trimmed builds and just kept the images, .git and source tree for things
+older than a week, it's still a ridiculous amount of identical data.
+
+I tried ZFS on linux for a while and got a superb amount of deduplication
+with that, but for my taste ZFS is far to memory consumptive and unstable
+to be trusted in linux at this time, so I went back to using XFS.
+
+I'd had a similar problem to solve when trying to keep online every single
+version of the linux kernel and had written some shell scripts to just
+do hard links with the previous lkml version if the files were identical.
+
+For this, I took a different approach and decided that md5sum was
+an adequate file fingerprint mechanism. The idea here is to walk a
+base directory and for each regular file in a file path, look up in a
+dictionary whether it exists. If not, do an md5sum on it and put it in
+the dictionary. If the md5sum name is a file under the special directory
+under the base directory exist, remove the path link and replace it with
+a hardlink to this file, else create one with a hard link. Rather than
+250 versions of Makefile in the top of each nearly identical build tree,
+one suffices with hardlinks into the directories keeping things the same.
+
+There are obvious limitations here. The timestamp is the most obvious
+one. Because there was already a policy in place of deleting build
+data for builds over a week old, doing this only on week old or older
+trees made sense- the weren't going to be rebuilt out of this source
+tree anyway. The trees stayed populated with files and the git repo
+was intact so, for forensic purposes, you could biseect some problem
+to this release and look at the state of the source for it (the whole
+object of this exercise, really). If you needed to rebuild and modify,
+you can do that elsewhere.
+
+There are other obvious problems. If you remove some object file or
+otherwise unlink things, the dictionary gets some challenges. I've mostly
+dealt with that by checking for the hash named common file for having
+a single link- removing that and cleaning it from the database.
---
+++ fake-dedup.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2020 by Matthew Jacob
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of The Version 2 GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+import hashlib
+import os
+import time
+import sys
+import shelve
+from stat import *
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print ("usage: rdlink root-directory")
+        sys.exit(1)
+
+    dbdir =  "./.rdlinkdb"
+
+    os.chdir(sys.argv[1])
+
+    try:
+        os.mkdir(dbdir)
+    except:
+        pass
+
+    cutoff = int(time.time()) - (7 * 24 * 60 * 60)
+    pd = shelve.open(dbdir + "/path2hash", flag='c')
+    toplev = True
+
+    for (dirpath, dirnames, filenames) in os.walk("."):
+        if toplev:
+            for d in dirnames:
+                path = os.path.join(dirpath, d)
+                dirstat = os.stat(path, follow_symlinks=False)
+                if dirstat.st_mtime > cutoff:
+                    print ("skipping", path, "because it is too new")
+                    del dirnames[dirnames.index(d)]
+            toplev = False
+            continue
+        if dirpath == dbdir:
+            continue
+        for f in filenames:
+            path = os.path.join(dirpath, f)
+            filestat = os.stat(path, follow_symlinks=False)
+            try:
+                hash = str(pd[path])
+            except:
+                hash = None
+            hashstat = None
+            if hash is None:
+                if not S_ISREG(filestat.st_mode):
+                    continue
+                hash = hashlib.md5(open(path, "rb").read()).hexdigest()
+                pd[path] = hash
+                file = dbdir + '/' + hash
+                try:
+                    os.stat(dbdir + '/' + hash, follow_symlinks=False)
+                except:
+                    os.link(path, file, follow_symlinks=False)
+                    os.utime(path, (filestat.st_mtime, filestat.st_atime))
+                    print ("create hash", hash, "for", path)
+                    continue
+
+            # The hash is in the database and the hash file is present
+            hashstat = os.stat(dbdir + '/' + hash, follow_symlinks=False)
+            if filestat.st_ino != hashstat.st_ino:
+                print ("add", path, "to", hash)
+                os.unlink(path)
+                os.link(dbdir + '/' + hash, path, follow_symlinks=False)
+                os.utime(path, (hashstat.st_mtime, hashstat.st_atime))
+
+    nukelist = []
+    for (dirpath, dirnames, filenames) in os.walk(dbdir):
+        for f in filenames:
+            if f != "path2hash":
+                path = os.path.join(dirpath, f)
+                stat = os.stat(path, follow_symlinks=False)
+                if stat.st_nlink == 1:
+                    print (path, "has only one link")
+                    nukelist.append(f)
+                    os.unlink(path)
+    for (k, v) in pd.items():
+        if str(v) in nukelist:
+            print("removing", k, "for hash", v)
+            del pd[k]
