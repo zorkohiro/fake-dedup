6459b508b0eb023c15a91a9d1859c3453d98a02a
Matthew Jacob <github@feral.com>
2020-02-23 19:56:26 -0800

    Final settled changes.

--- fake-dedup.py
+++ fake-dedup.py
@@ -27,13 +27,17 @@ if __name__ == "__main__":
         print("usage:", sys.argv[0], "root-directory")
         sys.exit(1)
 
+    past = 7 * 24 * 60 * 60
+    s = os.getenv("DEDUP_TIME")
+    if s is not None:
+        past = int(s)
 
     nadded = 0
     ncreat = 0
     nremvd = 0
     nskipd = 0
+    npassd = 0
 
-    debug = os.getenv("DEBUG")
     os.chdir(sys.argv[1])
     dbdir =  sys.argv[1] + "/.rdlinkdb"
 
@@ -43,27 +47,44 @@ if __name__ == "__main__":
         pass
 
     t = int(time.time())
-    cutoff = t - (7 * 24 * 60 * 60)
+    cutoff = t - past
+    marking = ".deduped"
 
     #
-    # Generate a list of candidate directories to descend
+    # Generate a list of tuples of candidate directories and mtimes to descend
     #
     topdirs = []
     for entry in os.scandir():
-        if entry.name.startswith('.') or entry.is_file():
+        if entry.name.startswith('.') or not entry.is_dir():
+            topdirs.append((entry.name, False, 0, 0))
             continue
+        atime = int(entry.stat().st_mtime)
         mtime = int(entry.stat().st_mtime)
         if mtime > cutoff:
-            if debug is not None:
-                print("skipping top directory", entry.name, "because it is too new")
+            print("skipping top directory", entry.name, "because it is too new")
             nskipd = nskipd + 1
-        else:
-            if debug is not None:
-                print("Will descend into", entry.name)
-            topdirs.append(entry.name)
+            topdirs.append((entry.name, False, mtime, atime))
+            continue
+        #
+        # See if we've already been here to traverse this directory.
+        # If we have, unless a FORCE environment variable is set, skip
+        # going through this again.
+        #
+        try:
+            if os.getenv("DEDUP_FORCE") is None:
+                filestat = os.stat(entry.name + "/" + marking)
+                print("skipping top directory", entry.name, "because we've already been here")
+                npassd = npassd + 1
+                topdirs.append((entry.name, False, mtime, atime))
+                continue
+        except:
+            pass
+        print("Will descend into", entry.name)
+        topdirs.append((entry.name, True, mtime, atime))
 
-    for topdir in topdirs:
-        topstat = os.stat(topdir)
+    for topdir, dodescend, _, _ in topdirs:
+        if not dodescend:
+            continue
         for (dirpath, dirnames, filenames) in os.walk(topdir):
             dirstat = os.stat(dirpath)
             assert(S_ISDIR(dirstat.st_mode))
@@ -78,8 +99,7 @@ if __name__ == "__main__":
                     hashstat = os.stat(file)
                     if filestat.st_ino != hashstat.st_ino:
                         nadded = nadded + 1
-                        if debug is not None:
-                            print("add", hash, "with", path)
+                        print("add to", hash, "with", path)
                         #
                         # Remove this file and link to the common link point.
                         # This is where we start reclaiming disk space.
@@ -92,14 +112,16 @@ if __name__ == "__main__":
                             os.utime(path, (filestat.st_mtime, filestat.st_atime))
                 except:
                     ncreat = ncreat + 1
-                    if debug is not None:
-                        print("create hash", hash, "for", path)
+                    print("create hash", hash, "for", path)
                     #
                     # First time we've encountered this hash- create the common link point
                     #
                     os.link(path, file)
-        # restore any timestamps to the top directory
-        os.utime(topdir, (topstat.st_mtime, topstat.st_atime))
+        #
+        # Mark that we've been here.
+        #
+        with open(topdir + "/" + marking, "wb") as f:
+            pass
 
     for (dirpath, dirnames, filenames) in os.walk(dbdir):
         for f in filenames:
@@ -107,11 +129,19 @@ if __name__ == "__main__":
             stat = os.stat(path)
             if stat.st_nlink == 1:
                 nremvd = nremvd + 1
-                if debug is not None:
-                    print(path, "has only one link so removing")
+                print(path, "has only one link so removing")
                 os.unlink(path)
+
+    # restore any timestamps to the top directories
+    for topdir, _, mtime, atime in topdirs:
+        if mtime != 0:
+            try:
+                os.utime(topdir, (mtime, atime))
+            except:
+                print("unable to set time on", topdir)
 
     print(nadded, "paths added to existing hash files")
     print(ncreat, "new hashes created")
     print(nremvd, "orphaned hash files removed")
-    print(nskipd, "top directories skipped")
+    print(nskipd, "top directories skipped because they were too new")
+    print(npassd, "top directories skipped because we've already incorporated them")
